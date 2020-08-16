e82463c7ef00e5a11414c6fe2c4ef50ee436cbfe
jarvi652 <jarviskatana@gmail.com>
2020-08-16 18:45:04 +0300

    otherchanges

--- js/index.js
+++ js/index.js
@@ -6,32 +6,27 @@ function validate(){
     var Lastname= document.getElementById("Last Name")
     var businessType= document.getElementById("businessType")
    
-    if(Email.value===""){
+    if(Email.value==="" && phoneNumber.value===""){
         document.getElementById("error-email").innerHTML="This field is required";
         Email.focus();
-        return false;        
-    }
-    else if(phoneNumber.value===""){
         document.getElementById("error-phone").innerHTML="This field is required";
         phoneNumber.focus();
         return false;        
-    }
-    else if(Firstname.value===""){
+    }else if(Firstname.value==="" && Lastname.value===""){
         document.getElementById("error-fname").innerHTML="This field is required";
         Firstname.focus();
-        return false;        
-    }
-    else if(Lastname.value===""){
         document.getElementById("error-lname").innerHTML="This field is required";
         Lastname.focus();
-        return false;        
-    }
-    else if(businessType.value < 1){
+        return false;
+
+    }else if(businessType.value < 1){
         document.getElementById("error-btype").innerHTML="This field is required";
         businessType.focus();
-        return false;        
+        return false;
     }
     else{
         return true;
     }
+    
+    
 }
